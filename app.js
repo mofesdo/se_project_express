51783723681a371a5ca6e999bb1be103f36e31d9
Modesto <mofesdo.dev@gmail.com>
2025-05-06 01:23:38 -0700

    Fix: Eslint now allows console.log

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const {PORT = 3001} = process.env;
+
+mongoose
+    .connect('mongodb://127.0.0.1:27017/wtwr_db')
+    .then(()=> {
+        console.log('connected to DB');
+    })
+    .catch((e) => console.error(e));
 
 app.listen(PORT, ()=> {
     console.log(`listening on port ${PORT}`);
