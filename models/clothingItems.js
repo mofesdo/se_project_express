d754d1e1ddd14846ecdaa33411171ad534c1559b
Modesto <mofesdo.dev@gmail.com>
2025-05-07 04:26:22 -0700

    Feat: Added owner, likes and createdAt properties to clothingItem schema

--- models/clothingItems.js
+++ models/clothingItems.js
@@ -13,6 +13,8 @@ const clothingItem = new mongoose.Schema({
     },
   },
   owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User", default: [] }],
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("clothingItems", clothingItem);
