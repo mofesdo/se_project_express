40faa26ba9741750d9163c43fd1a3ccaa2fef327
Modesto <mofesdo.dev@gmail.com>
2025-05-19 01:21:20 -0700

    Feat: CreateUser now reads email and password and hashes passwords

--- controllers/users.js
+++ controllers/users.js
@@ -1,49 +1,70 @@
-const User = require('../models/user');
-const { INVALID_DATA_ERROR_CODE, NO_DATA_ERROR_CODE, DEFAULT_ERROR_CODE } = require("../utils/errors");
-
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
+const {
+  INVALID_DATA_ERROR_CODE,
+  NO_DATA_ERROR_CODE,
+  DEFAULT_ERROR_CODE,
+} = require("../utils/errors");
 
 const getUsers = (req, res) => {
-    User.find({})
-        .then((users)=>{
-            res.send(users)
-        })
-        .catch((err) => {
-            console.error(err);
-            return res.status(DEFAULT_ERROR_CODE).send({message: 'An error has occurred on the server'})
-        })
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res
+        .status(DEFAULT_ERROR_CODE)
+        .send({ message: "An error has occurred on the server" });
+    });
 };
 
 const createUser = (req, res) => {
-    const {name, avatar} = req.body;
-    User.create({name, avatar})
-        .then((user) => {
-            res.status(201).send(user);
-        })
-        .catch((err) => {
-            console.error(err);
-            if(err.name === "ValidationError"){
-                return res.status(INVALID_DATA_ERROR_CODE).send({message: 'Invalid data entered'});
-            }
-            return res.status(DEFAULT_ERROR_CODE).send({message: 'An error has occurred on the server'});
-        });
+  const { name, avatar, email, password } = req.body;
+  bcrypt.hash(password, 10).then((hashedPassword) => {
+    User.create({ name, avatar, email, password: hashedPassword })
+      .then((user) => {
+        res.status(201).send(user);
+      })
+      .catch((err) => {
+        console.error(err);
+        if (err.name === "ValidationError") {
+          return res
+            .status(INVALID_DATA_ERROR_CODE)
+            .send({ message: "Invalid data entered" });
+        }
+        return res
+          .status(DEFAULT_ERROR_CODE)
+          .send({ message: "An error has occurred on the server" });
+      });
+  }).catch((err) => {
+    console.error(err);
+    return res.status( DEFAULT_ERROR_CODE).send({message: "An error has occured on the server"});
+  })
 };
 
 const getUser = (req, res) => {
-    const {userId} = req.params;
-    User.findById(userId)
-        .orFail()
-        .then((user) => {
-            res.status(200).send(user);
-        })
-        .catch((err) => {
-            console.error(err);
-            if(err.name === "DocumentNotFoundError"){
-                return res.status(NO_DATA_ERROR_CODE).send({message: 'Item not found'});
-            }
-            if(err.name === "CastError"){
-                return res.status(INVALID_DATA_ERROR_CODE).send({message: 'Invalid data entered'});
-            }
-            return res.status(DEFAULT_ERROR_CODE).send({message: 'An error has occurred on the server'});
-        });
-}
-module.exports = {getUsers, createUser, getUser};
+  const { userId } = req.params;
+  User.findById(userId)
+    .orFail()
+    .then((user) => {
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "DocumentNotFoundError") {
+        return res
+          .status(NO_DATA_ERROR_CODE)
+          .send({ message: "Item not found" });
+      }
+      if (err.name === "CastError") {
+        return res
+          .status(INVALID_DATA_ERROR_CODE)
+          .send({ message: "Invalid data entered" });
+      }
+      return res
+        .status(DEFAULT_ERROR_CODE)
+        .send({ message: "An error has occurred on the server" });
+    });
+};
+module.exports = { getUsers, createUser, getUser };
--- models/user.js
+++ models/user.js
@@ -12,21 +12,22 @@ const userSchema = new mongoose.Schema({
       },
       message: "You must enter a valid URL",
     },
-    email: {
-      type: String,
-      unique: true,
-      required: true,
-      validate: {
-        validator(v) {
-          return validator.isURL(v);
-        },
-        message: "You must enter a valid URL",
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
       },
+      message: "You must enter a valid email",
     },
-    password: {
-      type: String,
-      required: true,
-    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false
   },
 });
 
