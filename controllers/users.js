e7c089d1117feb0fcd19313e2892d74c5b253d44
Modesto <mofesdo.dev@gmail.com>
2025-05-19 17:29:59 -0700

    Feat: Added login controller and findUserByCredentials function in models/users

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
 const User = require("../models/user");
 const {
   INVALID_DATA_ERROR_CODE,
@@ -6,6 +7,7 @@ const {
   DEFAULT_ERROR_CODE,
   DUPLICATE_ERROR_CODE,
 } = require("../utils/errors");
+const {JWT_SECRET} = require("../utils/config");
 
 const getUsers = (req, res) => {
   User.find({})
@@ -22,29 +24,36 @@ const getUsers = (req, res) => {
 
 const createUser = (req, res) => {
   const { name, avatar, email, password } = req.body;
-  bcrypt.hash(password, 10).then((hashedPassword) => {
-    User.create({ name, avatar, email, password: hashedPassword })
-      .then((user) => {
-        res.status(201).send(user);
-      })
-      .catch((err) => {
-        console.error(err);
-        if (err.name === "ValidationError") {
+  bcrypt
+    .hash(password, 10)
+    .then((hashedPassword) => {
+      User.create({ name, avatar, email, password: hashedPassword })
+        .then((user) => {
+          res.status(201).send(user);
+        })
+        .catch((err) => {
+          console.error(err);
+          if (err.name === "ValidationError") {
+            return res
+              .status(INVALID_DATA_ERROR_CODE)
+              .send({ message: "Invalid data entered" });
+          }
+          if (err.code === 11000) {
+            return res
+              .status(DUPLICATE_ERROR_CODE)
+              .send({ message: "This user already exists" });
+          }
           return res
-            .status(INVALID_DATA_ERROR_CODE)
-            .send({ message: "Invalid data entered" });
-        }
-        if(err.code === 11000){
-            return res.status(DUPLICATE_ERROR_CODE).send({message: "This user already exists"})
-        }
-        return res
-          .status(DEFAULT_ERROR_CODE)
-          .send({ message: "An error has occurred on the server" });
-      });
-  }).catch((err) => {
-    console.error(err);
-    return res.status( DEFAULT_ERROR_CODE).send({message: "An error has occured on the server"});
-  })
+            .status(DEFAULT_ERROR_CODE)
+            .send({ message: "An error has occurred on the server" });
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res
+        .status(DEFAULT_ERROR_CODE)
+        .send({ message: "An error has occured on the server" });
+    });
 };
 
 const getUser = (req, res) => {
@@ -71,4 +80,22 @@ const getUser = (req, res) => {
         .send({ message: "An error has occurred on the server" });
     });
 };
-module.exports = { getUsers, createUser, getUser };
+
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      // authentication successful! user is in the user variable
+      res.send({
+       token: jwt.sign({ _id: user._id }, JWT_SECRET, {
+        expiresIn: "7d",
+      })
+      })
+    })
+    .catch((err) => {
+      // authentication error
+      res.status(401).send({ message: err.message });
+    });
+};
+module.exports = { getUsers, createUser, getUser, login };
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, minLength: 2, maxLength: 30 },
@@ -27,8 +28,29 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    select: false
+    select: false,
   },
 });
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password
+) {
+  // trying to find the user by email
+  return this.findOne({ email }) // this — the User model
+    .then((user) => {
+      // not found - rejecting the promise
+      if (!user) {
+        return Promise.reject(new Error("Incorrect email or password"));
+      }
+
+      // found - comparing hashes
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("incorrect email or password"));
+        }
+        return user;
+      });
+    });
+};
 
 module.exports = mongoose.model("user", userSchema);
