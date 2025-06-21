7da0ddcf054d5a241afdc1e10e3cc6e2a754da40
Modesto <mofesdo.dev@gmail.com>
2025-06-21 00:11:14 -0700

    Refactor: Changed .catch blocks to only next(err) to let middleware handle all errors.

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -8,7 +8,7 @@ const {
 
 const errorHandler = require('../middlewares/error-handler');
 
-const createItem = (req, res) => {
+const createItem = (req, res, next) => {
   const { name, weather, imageUrl } = req.body;
   const owner = req.user._id;
 
@@ -19,28 +19,30 @@ const createItem = (req, res) => {
     })
     .catch((err) => {
       // .error(err);
-      if (err.name === "ValidationError") {
-        return res
-          .status(INVALID_DATA_ERROR_CODE)
-          .send({ message: "Invalid data entered" });
-      }
-      return res
-        .status(DEFAULT_ERROR_CODE)
-        .send({ message: "An error has occurred on the server" });
+      // if (err.name === "ValidationError") {
+      //   // return res
+      //   //   .status(INVALID_DATA_ERROR_CODE)
+      //   //   .send({ message: "Invalid data entered" });
+      // }
+      // return res
+      //   .status(DEFAULT_ERROR_CODE)
+      //   .send({ message: "An error has occurred on the server" });
+      next(err);
     });
 };
 
-const getItems = (req, res) => {
+const getItems = (req, res, next) => {
   ClothingItem.find({})
     .then((items) => res.status(200).send(items))
     .catch((err) => {
-      console.error(err);
-      return res
-        .status(DEFAULT_ERROR_CODE)
-        .send({ message: "An error has occurred on the server" });
+      // console.error(err);
+      // return res
+      //   .status(DEFAULT_ERROR_CODE)
+      //   .send({ message: "An error has occurred on the server" });
+      next(err);
     });
 };
-const deleteItem = (req, res) => {
+const deleteItem = (req, res, next) => {
   const { itemId } = req.params;
   ClothingItem.findById(itemId)
     .orFail()
@@ -55,24 +57,25 @@ const deleteItem = (req, res) => {
       });
     })
     .catch((err) => {
-      console.error(err);
-      if (err.name === "DocumentNotFoundError") {
-        return res
-          .status(NO_DATA_ERROR_CODE)
-          .send({ message: "Item not found" });
-      }
-      if (err.name === "CastError") {
-        return res
-          .status(INVALID_DATA_ERROR_CODE)
-          .send({ message: err.message });
-      }
-      return res
-        .status(DEFAULT_ERROR_CODE)
-        .send({ message: "An error has occurred on the server" });
+      // console.error(err);
+      // if (err.name === "DocumentNotFoundError") {
+      //   return res
+      //     .status(NO_DATA_ERROR_CODE)
+      //     .send({ message: "Item not found" });
+      // }
+      // if (err.name === "CastError") {
+      //   return res
+      //     .status(INVALID_DATA_ERROR_CODE)
+      //     .send({ message: err.message });
+      // }
+      // return res
+      //   .status(DEFAULT_ERROR_CODE)
+      //   .send({ message: "An error has occurred on the server" });
+      next(err);
     });
 };
 
-const likeItem = (req, res) => {
+const likeItem = (req, res, next) => {
   ClothingItem.findByIdAndUpdate(
     req.params.itemId,
     { $addToSet: { likes: req.user._id } }, // add _id to the array if it's not there yet
@@ -83,23 +86,24 @@ const likeItem = (req, res) => {
       res.status(200).send({ data: item });
     })
     .catch((err) => {
-      console.error(err);
-      if (err.name === "DocumentNotFoundError") {
-        return res
-          .status(NO_DATA_ERROR_CODE)
-          .send({ message: "Item not found" });
-      }
-      if (err.name === "CastError") {
-        return res
-          .status(INVALID_DATA_ERROR_CODE)
-          .send({ message: err.message });
-      }
-      return res
-        .status(DEFAULT_ERROR_CODE)
-        .send({ message: "An error has occurred on the server" });
+      // console.error(err);
+      // if (err.name === "DocumentNotFoundError") {
+      //   return res
+      //     .status(NO_DATA_ERROR_CODE)
+      //     .send({ message: "Item not found" });
+      // }
+      // if (err.name === "CastError") {
+      //   return res
+      //     .status(INVALID_DATA_ERROR_CODE)
+      //     .send({ message: err.message });
+      // }
+      // return res
+      //   .status(DEFAULT_ERROR_CODE)
+      //   .send({ message: "An error has occurred on the server" });
+      next(err);
     });
 };
-const dislikeItem = (req, res) => {
+const dislikeItem = (req, res, next) => {
   ClothingItem.findByIdAndUpdate(
     req.params.itemId,
     { $pull: { likes: req.user._id } }, // remove _id from the array
@@ -110,20 +114,21 @@ const dislikeItem = (req, res) => {
       res.status(200).send({ data: item });
     })
     .catch((err) => {
-      console.error(err);
-      if (err.name === "DocumentNotFoundError") {
-        return res
-          .status(NO_DATA_ERROR_CODE)
-          .send({ message: "Item not found" });
-      }
-      if (err.name === "CastError") {
-        return res
-          .status(INVALID_DATA_ERROR_CODE)
-          .send({ message: err.message });
-      }
-      return res
-        .status(DEFAULT_ERROR_CODE)
-        .send({ message: "An error has occurred on the server" });
+      // console.error(err);
+      // if (err.name === "DocumentNotFoundError") {
+      //   return res
+      //     .status(NO_DATA_ERROR_CODE)
+      //     .send({ message: "Item not found" });
+      // }
+      // if (err.name === "CastError") {
+      //   return res
+      //     .status(INVALID_DATA_ERROR_CODE)
+      //     .send({ message: err.message });
+      // }
+      // return res
+      //   .status(DEFAULT_ERROR_CODE)
+      //   .send({ message: "An error has occurred on the server" });
+      next(err);
     });
 };
 
