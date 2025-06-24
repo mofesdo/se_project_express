63bb605b077efda09997351fda1f6a5f64480db5
Modesto <mofesdo.dev@gmail.com>
2025-06-24 06:06:24 -0700

    Fix: Added weather field to validateCardBody

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -19,6 +19,10 @@ module.exports.validateCardBody = celebrate({
       "string.empty": 'The "imageUrl" field must be filled in',
       "string.uri": 'the "imageUrl" field must be a valid url',
     }),
+    weather: Joi.string().valid("hot", "warm", "cold").required().messages({
+      "string.valid":
+        'The "weather" field must match to either hot, warm or cold',
+    }),
   }),
 });
 
@@ -58,7 +62,7 @@ module.exports.validateUserAuth = celebrate({
 module.exports.validateID = celebrate({
   params: Joi.object().keys({
     itemId: Joi.string().hex().length(24).messages({
-        "string.hex": 'The itemID is not in a valid format'
+      "string.hex": "The itemID is not in a valid format",
     }),
   }),
 });
